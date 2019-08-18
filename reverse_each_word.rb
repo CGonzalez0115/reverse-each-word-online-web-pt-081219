0ce304076591df95577ae5450a9e4d12e8f87b85
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 17:00:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,6 @@ def reverse_each_word(sentence2)
   reverse_each_word.reverse_word = "Hi again, just making sure it's reversed!"
     #=> "iH niaga, tsuj gnikam erus s'ti !desrever"
   end
+
+  reverse_each_word(sentence2).collect do
+  end
