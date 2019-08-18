ff6349121de3b6c2dddbd702c28d57d9d0092cf5
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 16:55:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,8 @@ def reverse_each_word(sentence1)
   (reverse_each_word.reverse_word).each do ("Hello there, and how are you?")
     #=> "olleH ,ereht dna woh era ?uoy"
   end
+
+def reverse_each_word(sentence2)
+  reverse_each_word.reverse_word = "Hi again, just making sure it's reversed!"
+    #=> "iH niaga, tsuj gnikam erus s'ti !desrever"
+  end
